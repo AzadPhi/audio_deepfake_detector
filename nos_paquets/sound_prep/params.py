4201c19796b26c00ebb2494dadee33020bee92be
Philippine Azadian <philippine.azadian@gmail.com>
2025-03-13 12:24:42 +0100

    modif du preprocessing

--- nos_paquets/sound_prep/params.py
+++ nos_paquets/sound_prep/params.py
@@ -14,3 +14,12 @@ PATH_TO_RAW_DATA=f'/home/{os.environ.get("USER_NAME")}/audio_deepfake_detector/r
 PATH_PROCESSED_DATA=f'/home/{os.environ.get("USER_NAME")}/audio_deepfake_detector/processed_data'
 
 #### CONSTANTS
+SAMPLING_RATE = 16000  # Fréquence d'échantillonnage (Hz) # on l'a fixé à 16000
+DURATION = 10.0  # Durée cible en secondes
+N_MELTS = 128  # Nombre de bandes de Mel
+
+HOP_LENGTH = 347 * DURATION  # Détermine le nombre de frames temporelles
+FMIN = 20  # Fréquence minimale
+FMAX = SAMPLING_RATE // 2  # Fréquence maximale (Nyquist)
+N_FFT = N_MELTS * 20  # Taille de la FFT
+SAMPLES = SAMPLING_RATE * DURATION
